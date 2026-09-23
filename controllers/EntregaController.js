5ff1850132222811a50bad66652736a4da24942c
Samuraii91 <davisilva.bg025@academico.ifsul.edu.br>
2026-09-23 16:36:19 -0300

    feat: alerta de estoque, entregas com multiplos produtos e acessibilidade no site (libras, audio e fonte)

--- controllers/EntregaController.js
+++ controllers/EntregaController.js
@@ -1,6 +1,14 @@
 import Entrega from '../models/Entrega.js'
 import Produto from '../models/Produto.js'
 
+function montarItens(body) {
+  const produtos = Array.isArray(body.produto) ? body.produto : (body.produto ? [body.produto] : [])
+  const quantidades = Array.isArray(body.quantidadeItem) ? body.quantidadeItem : (body.quantidadeItem ? [body.quantidadeItem] : [])
+  return produtos
+    .map((id, i) => id ? { produto: id, quantidade: Math.max(1, Number(quantidades[i]) || 1) } : null)
+    .filter(Boolean)
+}
+
 export default class EntregaController {
   async openAdd(req, res) {
     const produtos = await Produto.find()
@@ -8,9 +16,13 @@ export default class EntregaController {
   }
 
   async add(req, res) {
-    const { produto, cliente, endereco, status, data } = req.body
+    const { cliente, endereco, status, data } = req.body
+    const itens = montarItens(req.body)
+    if (!itens.length) {
+      return res.redirect('/entrega/add')
+    }
     await Entrega.create({
-      produto,
+      itens,
       cliente,
       endereco,
       status,
@@ -20,7 +32,7 @@ export default class EntregaController {
   }
 
   async list(req, res) {
-    const entregas = await Entrega.find().populate('produto')
+    const entregas = await Entrega.find().populate('itens.produto')
     res.render('entrega/lst', { entregas })
   }
 
@@ -31,9 +43,13 @@ export default class EntregaController {
   }
 
   async edt(req, res) {
-    const { produto, cliente, endereco, status, data } = req.body
+    const { cliente, endereco, status, data } = req.body
+    const itens = montarItens(req.body)
+    if (!itens.length) {
+      return res.redirect('/entrega/edt/' + req.params.id)
+    }
     await Entrega.findByIdAndUpdate(req.params.id, {
-      produto,
+      itens,
       cliente,
       endereco,
       status,
@@ -49,7 +65,7 @@ export default class EntregaController {
 
   async find(req, res) {
     const { cliente } = req.body
-    const entregas = await Entrega.find({ cliente: new RegExp(cliente, 'i') }).populate('produto')
+    const entregas = await Entrega.find({ cliente: new RegExp(cliente, 'i') }).populate('itens.produto')
     res.render('entrega/lst', { entregas })
   }
 }
--- models/Entrega.js
+++ models/Entrega.js
@@ -1,7 +1,12 @@
 import mongoose from 'mongoose'
 
-const entregaSchema = new mongoose.Schema({
+const itemEntregaSchema = new mongoose.Schema({
   produto: { type: mongoose.Schema.Types.ObjectId, ref: 'Produto', required: true },
+  quantidade: { type: Number, required: true, default: 1 }
+}, { _id: false })
+
+const entregaSchema = new mongoose.Schema({
+  itens: { type: [itemEntregaSchema], default: [] },
   cliente: { type: String, required: true },
   endereco: { type: String, required: true },
   status: { type: String, enum: ['Pendente', 'Em rota', 'Entregue'], default: 'Pendente' },
